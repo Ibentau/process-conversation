0f1c0c700268db968adcf566be3eb0ef3b293b5f
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-04-04 19:34:37 +0200

    feat: urls

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,14 @@
 </svelte:head>
 
 <header class="shadow-sm">
-	<div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+	<div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
 		<h1 class="text-lg leading-6 font-semibold">NLU data processing</h1>
+		<div class="flex gap-2">
+			<a href="/" class="btn btn-ghost">Home</a>
+			<a href="/stats" class="btn btn-ghost">Stats</a>
+			<a href="/export" class="btn btn-ghost">Export</a>
+		</div>
+
 	</div>
 </header>
 <main>
